867da33a7421c956affd2e45a7439569a6720f43
Stephen Rosso <stephenprosso@gmail.com>
2019-09-11 13:05:52 -0700

    PTLBrowse

--- New folder/simpleApp/ConsolidationBrowse.aspx.cs
+++ New folder/simpleApp/ConsolidationBrowse.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevExpress.Web;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,33 @@ namespace simpleApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            HeaderGridview.DataBind();
+            DetailDS.SelectParameters["Location"].DefaultValue = FocusedLocation();
+
+        }
+
+        protected void DetailGridview_CustomCallback(object sender, DevExpress.Web.ASPxGridViewCustomCallbackEventArgs e)
+        {
+            DetailDS.SelectParameters["Location"].DefaultValue = FocusedLocation();
+            DetailGridview.DataBind();
+        }
+
+        private string FocusedLocation()
+        {
+            if (HeaderGridview.VisibleRowCount > 0 && HeaderGridview.FocusedRowIndex > -1)
+            {
+                return HeaderGridview.GetRowValues(HeaderGridview.FocusedRowIndex, "Location").ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+
+        protected void AllocationDetailGridview_BeforePerformDataSelect(object sender, EventArgs e)
+        {
+            Session["ATA_Seq_#"] = (sender as ASPxGridView).GetMasterRowFieldValues("ATA_Seq_#");
 
         }
     }
---
+++ New folder/simpleApp/PTLBrowse.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="PTLBrowse.aspx.cs" Inherits="simpleApp.PTLBrowse" %>
+
+<%@ Register Assembly="DevExpress.Web.v19.1, Version=19.1.5.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" Namespace="DevExpress.Web" TagPrefix="dx" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+
+   
+</asp:Content>
